494dc544c93df36367bec7e8f61fd5cbf19f8743
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-05-29 12:03:51 -0400

    Updates

---
+++ precache-manifest.5ec5513f0a11b2a5179b1aa005523378.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb2b013859626c47d04c2b8b9018e00",
+    "url": "/avengers-clicky-game/index.html"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/avengers-clicky-game/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "cee9ac64fb327dbdd7c9",
+    "url": "/avengers-clicky-game/static/js/main.777e8533.chunk.js"
+  },
+  {
+    "revision": "6dfe2d47414cc3e33d52",
+    "url": "/avengers-clicky-game/static/js/runtime~main.161e569d.js"
+  }
+]);
--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import Game from './Game';
-
-const App = () => <Game/>
-
-export default App;
